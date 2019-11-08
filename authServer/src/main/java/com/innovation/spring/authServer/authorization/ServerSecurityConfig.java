8710acb652842c2647434d6482e26b5ace1af99b
Sanuj <1.sanuj@gmail.com>
2019-11-08 10:30:29 +0530

    ldap confi

--- authServer/src/main/java/com/innovation/spring/authServer/authorization/ServerSecurityConfig.java
+++ authServer/src/main/java/com/innovation/spring/authServer/authorization/ServerSecurityConfig.java
@@ -8,16 +8,30 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 
 @Configuration
 @Order(1)
 public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
  
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth)
+//      throws Exception {
+//        auth.inMemoryAuthentication()
+//          .withUser("john").password(passwordEncoder().encode("123")).roles("USER").authorities("custom");
+//    }
     @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-      throws Exception {
-        auth.inMemoryAuthentication()
-          .withUser("john").password(passwordEncoder().encode("123")).roles("USER").authorities("custom");
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org")
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new LdapShaPasswordEncoder())
+                .passwordAttribute("userPassword");
     }
  
     @Override
